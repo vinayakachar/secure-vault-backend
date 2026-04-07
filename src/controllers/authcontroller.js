cc0afd45e7a87b82fbb99e9ccb0af9508451b064
Vinayaka R <vinayakarachar@gmail.com>
2026-04-07 16:58:03 +0530

    fix: add user existence check to prevent login crash

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -2,26 +2,50 @@ import bycrypt from 'bcrypt';
 
 let users=[];
 const registerUser=async(req,res)=>{
+    try{
     const {username,password}=req.body;
     if(!username || !password){
-        res.status(400).json({message:"Plese provide valid username and passoword"})
+         return res.status(400).json({message:"Plese provide valid username and passoword"})
     }
 
     const existedUser=users.find(user=>user.username===username);
     if(existedUser){
-        res.status(400).json({message:"User already exists"});
+        return res.status(400).json({message:"User already exists"});
     }
 
     const hashedpassword=await bycrypt.hash(password,10);
 
-    const newUser={username,password:hashedpassword};
+    const newUser={id:Date.now().toString(),username,password:hashedpassword};
     users.push(newUser);
 
-    res.status(201).json({message:"User registered successfully"});
+    res.status(201).json({message:"User registered successfully" });
+    }catch(error){
+        res.status(500).json({message:"Internal server error"});
+    }
+}
+
+const loginUser=async(req,res)=>{
+    try{
+        const {username,password}=req.body;
+        if(!username || !password){
+           return res.status(400).json({message:"Please provide valid username and password"});
+        }
+        const user=users.find(user=>user.username===username);
+       
+        if(!user ){
+            return res.status(400).json({message:"Invalid username or password"});
+        }
     
-
-
+         const isPasswordValid=await bycrypt.compare(password,user.password);
+       if(!isPasswordValid){
+           return res.status(400).json({message:"Invalid username or password"});
+        }
+         res.status(200).json({message:"Login successful"});
+    }
+        catch(error){
+            res.status(500).json({message:"server error",error:error.message});
+        }
 
 }
 
-export {registerUser};
+export {registerUser,loginUser};
--- src/routes/authroute.js
+++ src/routes/authroute.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { registerUser } from '../controllers/authcontroller.js';
+import { loginUser, registerUser } from '../controllers/authcontroller.js';
 
 const router=express.Router();
 
 router.post('/register',registerUser);
+router.post('/login',loginUser)
 
 export default router;
